a203c610271534a503ad9798987795f7d6655209
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-07-09 04:25:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7464128864:AAE1PIFzPw_lQ72ahXrVuFoNGtUC
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Svnexbot")
 BOT_TEXT = "MY 𝐄𝐗𝐓𝐑𝐀𝐂𝐓𝐎𝐑"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8118667253"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002799217873"))
